f804010c9e6b37840a35d638ba0136ee7ed8b473
masatora <atarashi.masatora@gmail.com>
2021-08-10 23:07:46 +0900

    Update nodejs-server

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -3,9 +3,80 @@
 var utils = require('../utils/writer.js');
 var Auth = require('../service/AuthService');
 
-module.exports.authPOST = function authPOST (req, res, next) {
+module.exports.v1AuthCallbackGithubGET = function v1AuthCallbackGithubGET (req, res, next) {
+  Auth.v1AuthCallbackGithubGET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.v1AuthCallbackGoogleGET = function v1AuthCallbackGoogleGET (req, res, next) {
+  Auth.v1AuthCallbackGoogleGET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.v1AuthCallbackTwitterGET = function v1AuthCallbackTwitterGET (req, res, next) {
+  Auth.v1AuthCallbackTwitterGET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.v1AuthLoginGithubGET = function v1AuthLoginGithubGET (req, res, next) {
+  Auth.v1AuthLoginGithubGET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.v1AuthLoginGoogleGET = function v1AuthLoginGoogleGET (req, res, next) {
+  Auth.v1AuthLoginGoogleGET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.v1AuthLoginPOST = function v1AuthLoginPOST (req, res, next) {
   var body = req.swagger.params['body'].value;
-  Auth.authPOST(body)
+  Auth.v1AuthLoginPOST(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.v1AuthLoginTwitterGET = function v1AuthLoginTwitterGET (req, res, next) {
+  Auth.v1AuthLoginTwitterGET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.v1AuthPOST = function v1AuthPOST (req, res, next) {
+  var body = req.swagger.params['body'].value;
+  Auth.v1AuthPOST(body)
     .then(function (response) {
       utils.writeJson(res, response);
     })
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var fs = require('fs'),
     http = require('http');
 
 var app = require('connect')();
+var cors = require('cors');
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
 var serverPort = process.env.PORT || 8080;
@@ -22,6 +23,9 @@ var swaggerDoc = jsyaml.safeLoad(spec);
 
 // Initialize the Swagger middleware
 swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+
+
+  app.use(cors());
 
   // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
   app.use(middleware.swaggerMetadata());
